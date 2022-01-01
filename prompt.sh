6649aa3880de7a595a551a2b5debf31c11217fe6
Rafael Kitover <rkitover@gmail.com>
2022-01-01 01:26:44 +0000

    Put space back after $MSYSTEM, shorten hostname.
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- prompt.sh
+++ prompt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-_sps_hostname=$(hostname)
+_sps_hostname=$(hostname | sed 's/\..*//')
 _e=$(printf "\033")
 _esc= _end=
 [ -n "$BASH_VERSION" ]  && _esc=$(printf '\001') _end=$(printf '\002')
@@ -29,9 +29,8 @@ _SPS_in_msys2() {
 
 _SPS_msystem() {
     if [ -n "$MSYSTEM" ] && _SPS_in_msys2; then
-        # need trailing space here
-#        printf "${_esc}${_e}[1;97m<${_e}[0;95m${_end}%s${_esc}${_e}[1;97m>${_end} " "$MSYSTEM"
-        printf "${_esc}${_e}[0;95m${_end}%s" "$MSYSTEM"
+        # Need trailing space in case it's empty.
+        printf "${_esc}${_e}[0;95m${_end}%s " "$MSYSTEM"
     fi
 }
 
@@ -87,7 +86,7 @@ _SPS_cwd() {
 
 if [ -z "$ZSH_VERSION" ]; then
 
-    PS1='`_SPS_cmd_status` `_SPS_msystem` `_SPS_cwd` `_SPS_git_branch`
+    PS1='`_SPS_cmd_status` `_SPS_msystem``_SPS_cwd` `_SPS_git_branch`
 '"${_esc}${_e}[38;2;140;206;250m${_end}${USER}${_esc}${_e}[1;97m${_end}@${_esc}${_e}[0m${_e}[38;2;140;206;250m${_end}${_sps_hostname} ${_esc}${_e}[38;2;220;20;60m${_end}>${_esc}${_e}[0m${_end} "
 
 else # zsh
@@ -95,7 +94,7 @@ else # zsh
     setopt PROMPT_SUBST
 
     precmd() {
-        echo "$(_SPS_cmd_status) $(_SPS_msystem) $(_SPS_cwd) $(_SPS_git_branch)"
+        echo "$(_SPS_cmd_status) $(_SPS_msystem)$(_SPS_cwd) $(_SPS_git_branch)"
     }
 
     PS1="%{${_e}[38;2;140;206;250m%}${USER}%{${_e}[1;97m%}@%{${_e}[0m${_e}[38;2;140;206;250m%}${_sps_hostname} %{${_e}[38;2;220;20;60m%}>%{${_e}[0m%} "
